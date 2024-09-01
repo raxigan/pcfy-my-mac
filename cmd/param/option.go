56d1acc4dd6b485a84a26933e57e2197f79711d6
raxigan <raxigan@gmail.com>
2024-09-01 19:52:04 +0200

    #15 Add Android Studio and PyCharm to supported IDEs, change IDE naming in params

--- cmd/param/option.go
+++ cmd/param/option.go
@@ -37,15 +37,23 @@ func IntelliJ() IDE {
 func IntelliJCE() IDE {
 	return IDE{
 		KeymapsDir:     "Library/Application Support/JetBrains/IdeaIC{version}/keymaps",
-		FullName:       "IntelliJ IDEA CE",
+		FullName:       "IntelliJ IDEA Community Edition",
+		SrcKeymapsFile: "idea.xml",
+	}
+}
+
+func PyCharmCE() IDE {
+	return IDE{
+		KeymapsDir:     "Library/Application Support/JetBrains/PyCharmCE{version}/keymaps",
+		FullName:       "PyCharm Community Edition",
 		SrcKeymapsFile: "idea.xml",
 	}
 }
 
 func PyCharm() IDE {
 	return IDE{
-		KeymapsDir:     "Library/Application Support/JetBrains/PyCharmCE{version}/keymaps",
-		FullName:       "PyCharm CE",
+		KeymapsDir:     "Library/Application Support/JetBrains/PyCharm{version}/keymaps",
+		FullName:       "PyCharm Professional Edition",
 		SrcKeymapsFile: "idea.xml",
 	}
 }
@@ -54,6 +62,14 @@ func GoLand() IDE {
 	return IDE{
 		KeymapsDir:     "Library/Application Support/JetBrains/GoLand{version}/keymaps",
 		FullName:       "GoLand",
+		SrcKeymapsFile: "idea.xml",
+	}
+}
+
+func AndroidStudio() IDE {
+	return IDE{
+		KeymapsDir:     "Library/Application Support/Google/AndroidStudio{version}/keymaps",
+		FullName:       "Android Studio",
 		SrcKeymapsFile: "idea.xml",
 	}
 }
@@ -67,7 +83,7 @@ func Fleet() IDE {
 	}
 }
 
-var IDEKeymaps = []IDE{IntelliJ(), IntelliJCE(), PyCharm(), GoLand(), Fleet()}
+var IDEKeymaps = []IDE{IntelliJ(), IntelliJCE(), PyCharm(), PyCharmCE(), GoLand(), AndroidStudio(), Fleet()}
 var SystemSettings = []string{
 	"Enable Dock auto-hide (2s delay)",
 	`Change Dock minimize animation to "scale"`,
